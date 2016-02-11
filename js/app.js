23e8ce55e7ab8ad4db8d9ca7bd9d8b675be237ce
sherrytherese <sherry.therese@gmail.com>
2016-02-11 08:48:35 -0800

    stuff i changed

--- js/app.js
+++ js/app.js
@@ -8,15 +8,38 @@ $(document).ready(function(){
 		$('.ryu-ready').hide();
 	})
 	.mousedown(function() {
-		//play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//animate hadouken to the right of the screen
+		$('.hadouken').finish().show().animate(
+			{'left':'1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left','619px');
+			});
 	})
 	.mouseup(function() {
-		$('ryu-ready').show();
+		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
-		//ryu goes back to his ready position
 	});
+	//.keydown("x",function() {
+	//alert("x was pressed");
+	//$('.ryu-ready').hide();
+	//$('.ryu-cool').show();
+	//});
 });
+$(document).keydown("x",function() {
+	//alert("x was pressed");
+	$('.ryu-ready').hide();
+	$('.ryu-cool').show();
+});
+$(document).keyup("x",function() {
+	$('.ryu-ready').show();
+	$('.ryu-cool').hide();
+});
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
